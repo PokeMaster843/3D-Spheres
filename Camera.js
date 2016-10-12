f7a0d080075283d46016102010e9eee9dfe84633
John Knickerbocker <PokeMaster843@gmail.com>
2016-10-12 07:25:40 -0400

    Update Camera.js

--- Camera.js
+++ Camera.js
@@ -1,5 +1,23 @@
 var Camera = function(coords, angles) {
   
+  this.rotateCamera = function(newAngles) {
+    
+    angles[0] += newAngles[0];
+    angles[1] += newAngles[1];
+    angles[2] += newAngles[2];
+    
+  };
   
+  this.translateCamera = function(newCoords) {
+    
+    coords[0] += newCoords[0];
+    coords[1] += newCoords[1];
+    coords[2] += newCoords[2];
+    
+  };
+  
+  this.addPerspective = function(obj) {
+    
+  };
   
 };
